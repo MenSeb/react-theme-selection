37733a2bae9493751f98080624f0d8c6dda249d3
MenSeb <sebastien.menard.github@gmail.com>
2019-04-17 23:31:28 -0400

    remove viewBox, moving to Palette

--- src/components/Defs/Defs.js
+++ src/components/Defs/Defs.js
@@ -5,7 +5,7 @@ export default function ThemeDefs ()
   return (
     <svg className='ThemeDefs' aria-hidden={ true }>
       <defs>
-        <symbol id='ThemeSquare' viewBox='0 0 4 4'>
+        <symbol id='ThemeSquare'>
           <path d='M0 0 h4 v1.1 h-4 z' />
           <path d='M0 1 h4 v1.1 h-4 z' />
           <path d='M0 2 h4 v1.1 h-4 z' />
