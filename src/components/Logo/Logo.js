01baaa3c7a9a07429fada58d6a162c91d021367c
MenSeb <sebastien.menard.github@gmail.com>
2019-04-18 15:32:25 -0400

    Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ThemeLogo ()
+{
+  return (
+    <img
+      className='ThemeLogo'
+      src='logo.svg'
+      alt='Theme Selection logo'
+    />
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import ThemeDefs from './Defs'
+import ThemeLogo from './Logo'
 import ThemePalette from './Palette'
 import ThemeSelection from './Selection'
 import ThemeViewer from './Viewer'
 
 export {
   ThemeDefs,
+  ThemeLogo,
   ThemePalette,
   ThemeSelection,
   ThemeViewer,
