98ec55291321c195b6ad59d40ed28f529b0a58e7
MenSeb <sebastien.menard.github@gmail.com>
2019-04-17 23:14:07 -0400

    remove fill attribute, moving to styles

--- src/components/Defs/Defs.js
+++ src/components/Defs/Defs.js
@@ -6,10 +6,10 @@ export default function ThemeDefs ()
     <svg className='ThemeDefs' aria-hidden={ true }>
       <defs>
         <symbol id='ThemeSquare' viewBox='0 0 4 4'>
-          <path fill='var( --color-background )' d='M0 0 h4 v1.1 h-4 z' />
-          <path fill='var( --color-border )' d='M0 1 h4 v1.1 h-4 z' />
-          <path fill='var( --color-color )' d='M0 2 h4 v1.1 h-4 z' />
-          <path fill='var( --color-hover )' d='M0 3 h4 v1 h-4 z' />
+          <path d='M0 0 h4 v1.1 h-4 z' />
+          <path d='M0 1 h4 v1.1 h-4 z' />
+          <path d='M0 2 h4 v1.1 h-4 z' />
+          <path d='M0 3 h4 v1 h-4 z' />
         </symbol>
       </defs>
     </svg>
