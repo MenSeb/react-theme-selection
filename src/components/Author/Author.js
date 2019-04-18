b545881c40e3eff3ed1e2f42f413e3d0629f09fa
MenSeb <sebastien.menard.github@gmail.com>
2019-04-18 16:32:17 -0400

    add classNames

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -4,14 +4,19 @@ export default function ThemeAuthor ()
 {
   return (
     <section className='ThemeAuthor'>
-      <p>
+      <p className='Text'>
         Made with
-        <svg viewBox='-10 -10 20 20'>
+        <svg className='Icon' viewBox='-10 -10 20 20'>
           <title>love</title>
           <path d='M-8 0 a 4 4 0 0 1 8 -8 a 4 4 0 0 1 8 8 l-8 8 z' />
         </svg>
         by
-        <a title='Github' href='https://github.com/MenSeb' target='_blank'>
+        <a
+          className='Link'
+          title='Github'
+          href='https://github.com/MenSeb'
+          target='_blank'
+        >
           MenSeb
         </a>
       </p>
