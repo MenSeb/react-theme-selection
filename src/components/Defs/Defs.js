2cee59bcbc2716c3184d567153836f39880cfadc
MenSeb <sebastien.menard.github@gmail.com>
2019-04-17 22:36:15 -0400

    hide blank between paths

--- src/components/Defs/Defs.js
+++ src/components/Defs/Defs.js
@@ -6,9 +6,9 @@ export default function ThemeDefs ()
     <svg className='ThemeDefs' aria-hidden={ true }>
       <defs>
         <symbol id='ThemeSquare' viewBox='0 0 4 4'>
-          <path d='M0 0 h4 v1 h-4 z' />
-          <path d='M0 1 h4 v1 h-4 z' />
-          <path d='M0 2 h4 v1 h-4 z' />
+          <path d='M0 0 h4 v1.1 h-4 z' />
+          <path d='M0 1 h4 v1.1 h-4 z' />
+          <path d='M0 2 h4 v1.1 h-4 z' />
           <path d='M0 3 h4 v1 h-4 z' />
         </symbol>
       </defs>
