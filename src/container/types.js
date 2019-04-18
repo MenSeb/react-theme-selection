9022c9b7d5b523dd6e2c1ab45daef19352449571
MenSeb <sebastien.menard.github@gmail.com>
2019-04-17 22:33:45 -0400

    update ThemeContainer validation prop types

--- src/container/types.js
+++ src/container/types.js
@@ -1,16 +1,37 @@
 import { arrayOf } from 'prop-types'
 
+const msgComponent = `Error from component ThemeContainer in prop themes`
+const msgArray = `Type of theme should be array`
+const msgLength = `Length of each theme should be 4`
+const msgString = `Each color in theme should be of type string`
+const msgHex = `Each color in theme should be a 3 or 6 length hex color code without #`
+const regexpHex = /^(?:(?:[a-f0-9]{6})|(?:[a-f0-9]{3}))$/i
+
+const msgTheme = ({ theme, index }) => `Invalid theme ${ theme } at index ${ index }`
+
+const msgError = ({ theme, index, message }) => new Error( `
+  ${ msgComponent }.
+  ${ msgTheme({ theme, index }) }.
+  ${ message }.
+` )
+
 export default {
   themes: arrayOf(
-    ( value, key, component, location, name ) =>
+    ( themes, index ) =>
     {
-      console.log({
-        value,
-        key,
-        component,
-        location,
-        name
-      });
+      const theme = themes[ index ]
+
+      if ( !Array.isArray( theme ) )
+        return msgError({ theme, index, message: msgArray })
+
+      if ( theme.length !== 4 )
+        return msgError({ theme, index, message: msgLength })
+
+      if ( theme.some( color => typeof color !== 'string' ) )
+        return msgError({ theme, index, message: msgString })
+
+      if ( theme.some( color => !regexpHex.test( color ) ) )
+        return msgError({ theme, index, message: msgHex })
     }
   ).isRequired
 }
