38516850bf36d4df3112f3d286c4ee94a8f21020
MenSeb <sebastien.menard.github@gmail.com>
2019-04-18 16:36:03 -0400

    add Title className

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ThemeBrand ()
 {
   return (
-    <h1 className='ThemeBrand'>
+    <h1 className='ThemeBrand Title'>
       Theme Selection
     </h1>
   )
