f98e33106f420ab39f86cda088d191399afd41bb
MenSeb <sebastien.menard.github@gmail.com>
2019-04-18 15:27:07 -0400

    Viewer component

---
+++ src/components/Viewer/Viewer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function ThemeViewer ()
+{
+  return (
+    <section className='ThemeViewer'>
+      <section className='ViewerWrapper'>
+        <h1 className='Title'>
+          Select any Theme Palette to update colors
+        </h1>
+        <h2 className='Title'>
+          Hover any text, title or scrollbar to change color
+        </h2>
+        <p className='Text'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus in justo aliquam, interdum turpis pharetra, aliquet magna.
+          Mauris a gravida tortor. Nullam feugiat varius dictum.
+          Integer consectetur, velit id condimentum molestie, quam mi faucibus massa, non tincidunt massa massa a turpis.
+          Duis pulvinar massa vitae nulla dictum ultricies.
+          Aliquam at mauris et arcu vehicula placerat hendrerit sed nulla.
+          Vivamus aliquam, lectus pellentesque elementum maximus, mauris leo feugiat arcu, quis venenatis augue felis id odio.
+          Pellentesque facilisis lacus vitae ipsum maximus, ut convallis nisi tempus.
+          Sed quis lobortis magna. Maecenas dignissim, enim vitae posuere semper, purus eros fringilla felis, sit amet convallis magna libero ac est.
+          Etiam sapien odio, pharetra vel sapien nec, faucibus commodo lectus.
+          Morbi at diam id quam pellentesque vehicula. Vestibulum vitae pharetra lectus.
+          Donec bibendum dictum felis, ut sollicitudin odio tincidunt et.
+          Phasellus malesuada enim sem, ac blandit ex condimentum nec.
+          Nunc dapibus sem vitae justo semper laoreet. Suspendisse potenti.
+          Donec vulputate rutrum augue, vitae iaculis massa sollicitudin ut.
+          Sed et venenatis arcu, a sagittis tellus.
+        </p>
+      </section>
+    </section>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import ThemeDefs from './Defs'
 import ThemePalette from './Palette'
 import ThemeSelection from './Selection'
+import ThemeViewer from './Viewer'
 
 export {
   ThemeDefs,
   ThemePalette,
   ThemeSelection,
+  ThemeViewer,
 }
