a68126ec99bf21eed08f12d2e260a1fd8c14321e
MenSeb <sebastien.menard.github@gmail.com>
2019-04-18 16:51:57 -0400

    update application structure with components, container and general layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import ThemeContainer from '../container'
+import { THEMES } from '../constants'
+import {
+  ThemeAuthor,
+  ThemeBrand,
+  ThemeLogo,
+  ThemeViewer
+} from '../components';
 
 export default function App ()
 {
   return (
     <div className='App'>
+      <header>
+        <ThemeLogo />
+        <ThemeBrand />
+      </header>
 
+      <main>
+        <ThemeViewer />
+        <ThemeContainer themes={ THEMES } />
+      </main>
 
-
+      <footer>
+        <ThemeAuthor />
+      </footer>
     </div>
   )
 }
